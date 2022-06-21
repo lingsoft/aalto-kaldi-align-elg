b77c970cb2421732e228a5ad57d838011be9b27c
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-06-21 13:47:39 +0300

    Change warnings to errors

--- app.py
+++ app.py
@@ -22,9 +22,13 @@ if not os.path.isdir(src_for_txt):
     os.makedirs(src_for_txt, exist_ok=True)
 
 SUPPORT_LANGS = ('fi', 'en', 'se', 'et', 'kv')
-
+AUDIO_FORMAT = "LINEAR16"
+SAMPLE_RATE = 16000
+MIN_FILE_SIZE = 20
+MAX_FILE_SIZE = 25600
 
 class AaltoAlign(FlaskService):
+
     def process_audio(self, request: AudioRequest):
         lang = self.url_param('lang_code')
         if lang not in SUPPORT_LANGS:
@@ -34,44 +38,38 @@ class AaltoAlign(FlaskService):
 
         audio_file = request.content
 
+        if request.format != AUDIO_FORMAT:
+            err_msg = StandardMessages.generate_elg_request_audio_format_unsupported(
+                    params=[request.format])
+            return Failure(errors=[err_msg])
+
         # validating file size
         audio_file_size = sys.getsizeof(audio_file) / 1024
-        if audio_file_size < 20:
+        if audio_file_size < MIN_FILE_SIZE:
             err_msg = StandardMessages.generate_elg_request_invalid(
                 detail={'audio': 'File is empty or too small'})
             return Failure(errors=[err_msg])
-        if audio_file_size > 25 * 1024:  # maximum allow is 25MB
+        if audio_file_size > MAX_FILE_SIZE: 
             err_msg = StandardMessages.generate_elg_upload_too_large(
                 detail={'audio': 'File is over 25MB'})
             return Failure(errors=[err_msg])
 
-        # validating file format
+        # validating file format and sample rate
         try:
             audio_info = WAVE(io.BytesIO(audio_file)).info
         except Exception:
-            err_msg = StandardMessages.generate_elg_request_invalid(
-                detail={'audio': 'Audio is not in WAV format'})
+            err_msg = StandardMessages.generate_elg_request_audio_format_unsupported(
+                    params=["different from WAV"])
             return Failure(errors=[err_msg])
-
-        # warning about the parameter if it's not match the sent file
-        format_warning_msg, sampleRate_warning_msg = None, None
-        if request.format != 'LINEAR16':
-            format_warning_msg = StatusMessage(
-                code='elg.request.parameter.format.value.mismatch',
-                params=['LINEAR16'],
-                text=
-                'Sent parameter format is not LINEAR16 although sent file is: {0}'
-            )
-
-        if hasattr(request, 'sample_rate'
-                   ) and request.sample_rate != audio_info.sample_rate:
-            sampleRate_warning_msg = StatusMessage(
-                code='elg.request.parameter.sampleRate.value.mismatch',
-                params=[str(audio_info.sample_rate)],
-                text=
-                'Sent parameter sample rate is not matched with the sample rate of sent file: {0}'
-            )
-
+        if hasattr(request, 'sample_rate') and request.sample_rate != SAMPLE_RATE:
+            err_msg = StandardMessages.generate_elg_request_audio_samplerate_unsupported(
+                    params=[request.sample_rate])
+            return Failure(errors=[err_msg])
+        if audio_info.sample_rate != SAMPLE_RATE:
+            err_msg = StandardMessages.generate_elg_request_audio_samplerate_unsupported(
+                    params=[audio_info.sample_rate])
+            return Failure(errors=[err_msg])
+        
         # checking transcript
         try:
             transcript = request.params['transcript']
@@ -110,15 +108,8 @@ class AaltoAlign(FlaskService):
         # Clean up all files
         utils.clean_up(audio_name)
 
-        warnings_msg_lst = []
-        if format_warning_msg:
-            warnings_msg_lst.append(format_warning_msg)
-        if sampleRate_warning_msg:
-            warnings_msg_lst.append(sampleRate_warning_msg)
-
         if is_success:
-            return AnnotationsResponse(annotations=result,
-                                       warnings=warnings_msg_lst)
+            return AnnotationsResponse(annotations=result)
         else:
             error = StandardMessages.generate_elg_service_internalerror(
                 params=[result])
