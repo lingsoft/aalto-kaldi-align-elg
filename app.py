8e3d444f828f1f95da3bf705923f42b4f892c49f
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-06-22 11:33:25 +0300

    Improve request validation

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ AUDIO_FORMAT = "LINEAR16"
 SAMPLE_RATE = 16000
 MIN_FILE_SIZE = 20
 MAX_FILE_SIZE = 25600
+MIN_TRANSCRIPT = 3
+
 
 class AaltoAlign(FlaskService):
 
@@ -36,14 +38,35 @@ class AaltoAlign(FlaskService):
                 params=[lang])
             return Failure(errors=[err_msg])
 
-        audio_file = request.content
+        # In specs sampleRate but in ELG Python SDK sample_rate
+        # https://european-language-grid.readthedocs.io/en/stable/all/A1_PythonSDK/Model.html#module-elg.model.request.AudioRequest
+        for elem in ["format", "sample_rate", "content", "params"]:
+            if (not hasattr(request, elem)) or (getattr(request, elem) is None):
+                mp_msg = "Missing property {}".format(elem)
+                err_msg = StandardMessages.generate_elg_request_invalid(
+                        detail={"request": mp_msg})
+                return Failure(errors=[err_msg])
 
+        try:
+            transcript = request.params['transcript']
+        except KeyError:
+            detail = {'transcript': 'No transcript was given'}
+            err_msg = StandardMessages.generate_elg_request_invalid(
+                detail=detail)
+            return Failure(errors=[err_msg])
+
+        if len(transcript) < MIN_TRANSCRIPT:
+            detail = {'transcript': 'Given transcript is too short'}
+            error = StandardMessages.generate_elg_request_invalid(
+                detail=detail)
+            return Failure(errors=[error])
         if request.format != AUDIO_FORMAT:
             err_msg = StandardMessages.generate_elg_request_audio_format_unsupported(
                     params=[request.format])
             return Failure(errors=[err_msg])
 
-        # validating file size
+        audio_file = request.content
+
         audio_file_size = sys.getsizeof(audio_file) / 1024
         if audio_file_size < MIN_FILE_SIZE:
             err_msg = StandardMessages.generate_elg_request_invalid(
@@ -54,14 +77,13 @@ class AaltoAlign(FlaskService):
                 detail={'audio': 'File is over 25MB'})
             return Failure(errors=[err_msg])
 
-        # validating file format and sample rate
         try:
             audio_info = WAVE(io.BytesIO(audio_file)).info
         except Exception:
             err_msg = StandardMessages.generate_elg_request_audio_format_unsupported(
                     params=["different from WAV"])
             return Failure(errors=[err_msg])
-        if hasattr(request, 'sample_rate') and request.sample_rate != SAMPLE_RATE:
+        if int(request.sample_rate) != SAMPLE_RATE:
             err_msg = StandardMessages.generate_elg_request_audio_samplerate_unsupported(
                     params=[request.sample_rate])
             return Failure(errors=[err_msg])
@@ -69,43 +91,19 @@ class AaltoAlign(FlaskService):
             err_msg = StandardMessages.generate_elg_request_audio_samplerate_unsupported(
                     params=[audio_info.sample_rate])
             return Failure(errors=[err_msg])
-        
-        # checking transcript
-        try:
-            transcript = request.params['transcript']
-        except KeyError:
-            detail = {'transcript': 'No transcript was given'}
-            error = StandardMessages.generate_elg_request_missing(
-                detail=detail)
-            return Failure(errors=[error])
-
-        # too short transcript
-        if len(transcript) < 3:
-            detail = {
-                'transcript':
-                'Given transcript is too short, perhaps wrong transcript'
-            }
-            error = StandardMessages.generate_elg_request_invalid(
-                detail=detail)
-            return Failure(errors=[error])
 
         audio_name = str(uuid.uuid4()) + '.wav'
         transcript_name = audio_name[:-4] + '.txt'
         audio_save_path = os.path.join(src_for_wav, audio_name)
         transcript_save_path = os.path.join(src_for_txt, transcript_name)
 
-        # saving audio file
         with open(audio_save_path, 'wb') as fp:
             fp.write(audio_file)
 
-        # saving transcript file
         with open(transcript_save_path, 'w') as fp:
             fp.write(transcript)
 
-        # Call pipeline utilty
         is_success, result = utils.predict(audio_name, lang)
-
-        # Clean up all files
         utils.clean_up(audio_name)
 
         if is_success:
